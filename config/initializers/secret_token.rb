4018dadc5efd1a60cf2d1b4a51acfe99b6de9af2
John Philip Dorado <dyeypidorado@dyeypidorado-laptop.(none)>
2012-04-14 00:04:34 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kasapi::Application.config.secret_token = '96fbd6a8fe6075361c0b5ffd6c26f088567618ac2396cae4d1f4a46db1a35a660668aedd72658f50fff373fbf14ead24a762f494f884d6e5ae9c9a099a6db505'
